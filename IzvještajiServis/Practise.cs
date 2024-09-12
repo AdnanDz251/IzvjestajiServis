3c245532d202148dad4945a82a0b263a1aa192e8
Adnan Džindo <adnan.dzindo@argon.ba>
2024-09-12 16:48:27 +0200

    Inital push

---
+++ IzvještajiServis/Practise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace IzvještajiServis
+{
+    public class Practise
+    {
+        private readonly Timer _timer;
+
+        public Practise()
+        {
+            _timer = new Timer(3000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] {DateTime.Now.ToString()};
+            Console.WriteLine("TestLine");
+            File.AppendAllLines(@"C:\Users\adnan.dzindo\Desktop\TimerThingy.txt", lines);
+        }
+
+        public void Start() 
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
--- IzvještajiServis/Program.cs
+++ IzvještajiServis/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace IzvještajiServis
 {
@@ -10,6 +11,24 @@ namespace IzvještajiServis
     {
         static void Main(string[] args)
         {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Practise>(s =>
+                {
+                    s.ConstructUsing(practise => new Practise());
+                    s.WhenStarted(practise => practise.Start());
+                    s.WhenStopped(practise => practise.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("PractiseService");
+                x.SetDisplayName("Sta the Briga");
+                x.SetDescription("Just Practising My Services.");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
         }
     }
 }
